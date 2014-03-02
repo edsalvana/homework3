38e78283b83e0638c89bd75375d1a50500600065
Ed Salvana <ed@visualgratis.com>
2014-03-02 15:53:59 -0800

    Initial Commit

---
+++ homework3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  homework3
+//
+//  Created by Ed Salvana on 3/2/14.
+//  Copyright (c) 2014 Ed Salvana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ homework3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  homework3
+//
+//  Created by Ed Salvana on 3/2/14.
+//  Copyright (c) 2014 Ed Salvana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
